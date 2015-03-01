1055c63f23ee921f671b7694423204cbef8e1ab1
Ryan Sturmer <ryansturmer@gmail.com>
2015-03-01 17:12:58 -0500

    Got topoplogical thinning working

---
+++ js/image.js
@@ -0,0 +1,242 @@
+var lut_a = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+var lut_b = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]
+var lut_c = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+var lut_d = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+var lut_e = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+var lut_f = [0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+var lut_g = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+var lut_h = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+function max(arr) {
+	retval = arr[0];
+	for(var i=1; i<arr.length; i++) {
+		retval = Math.max(arr[i], retval);
+	}
+	return retval;
+}
+
+function min(arr) {
+	retval = arr[0];
+	for(var i=1; i<arr.length; i++) {
+		retval = Math.min(arr[i], retval);
+	}
+	return retval;
+}
+
+function neighborhoodToMask(n,ne,e,se,s,sw,w,nw) {
+	return (n << 0) | (ne << 1) | (e << 2) | (se << 3) | (s << 4) | (sw << 5) | (w << 6) | (nw << 7);
+}
+
+function Img(arg1,arg2) {
+	function arrayFromCanvas(canvas) {
+		var ctx = canvas.getContext('2d');
+		var imgData = ctx.getImageData(0,0,canvas.width, canvas.height);
+		var data = imgData.data;
+		retval = [];
+		for(var i=0; i<data.length; i+=4) {
+			v = Math.max(data[i], data[i+1], data[i+2]);
+			retval.push(v);
+		}
+		return retval;
+	}
+
+	if(arg1 instanceof jQuery) {
+		canvas = arg1[0];
+		this.width = canvas.width;
+		this.height = canvas.height;
+		this.d = arrayFromCanvas(arg1);
+	} else if(arg1 instanceof HTMLCanvasElement) {
+		this.width = arg1.width;
+		this.height = arg1.height;
+		this.d = arrayFromCanvas(arg1);
+	} else if(arg1 instanceof CanvasRenderingContext2D) {
+		this.width = arg1.canvas.width;
+		this.height = arg1.canvas.height;
+		this.d = arrayFromCanvas(arg1.canvas);
+	} else if(arg1 !== undefined && arg2 !== undefined) {
+		this.width = arg1;
+		this.height = arg2;
+		this.d = [];
+		for(i=0; i<this.width*this.height; i++) {
+			this.d.push(0);
+		}
+	} else {
+		throw "I don't know how to make an image from this"
+	}
+}
+
+Img.prototype.drawOn = function(canvas) {
+	var ctx = canvas.getContext("2d");
+	var imgData = ctx.createImageData(this.width, this.height);
+	var data = imgData.data;
+	var i=0;
+	for(var j=0; j<data.length; j+=4) {
+		var v = this.d[i++];
+		data[j] = v;
+		data[j+1] = v;
+		data[j+2] = v;
+		data[j+3] = 0xff;
+	}
+	ctx.putImageData(imgData, 0,0);
+}
+
+Img.prototype.threshold = function(thresh, value) {
+	value = value || 0xff;
+	for(var i=0; i<this.d.length; i++) {
+		this.d[i] = this.d[i] >= thresh ? value : 0;
+	} 
+}
+
+Img.prototype.setPixel = function(x,y,val) {
+	this.d[x+y*this.width] = val;
+}
+
+Img.prototype.getPixel = function(x,y) {
+	return this.d[x+y*this.width];
+}
+
+Img.prototype._dilate = function() {
+	for(var x = 1; x<this.width-1; x++) {
+		for(var y=1; y<this.height-1; y++) {
+			center = this.getPixel(x,y);
+			if(center === 0x00) {
+				a = this.getPixel(x-1,y-1);
+				b = this.getPixel(x+0,y-1);
+				c = this.getPixel(x+1,y-1);
+				d = this.getPixel(x-1,y+0);
+				//e = this.getPixel(x,y);
+				f = this.getPixel(x+1,y+0);
+				g = this.getPixel(x-1,y+1);
+				h = this.getPixel(x+0,y+1);
+				i = this.getPixel(x+1,y+1);
+				var v = a | b | c | d | f | g | h | i;
+				if(v === 0xff) {
+					this.setPixel(x,y,1);
+				}
+			}
+		}
+	}
+}
+
+Img.prototype._erode = function() {
+	for(var x = 1; x<this.width-1; x++) {
+		for(var y=1; y<this.height-1; y++) {
+			center = this.getPixel(x,y);
+			if(center === 0xff) {
+				a = this.getPixel(x-1,y-1);
+				b = this.getPixel(x+0,y-1);
+				c = this.getPixel(x+1,y-1);
+				d = this.getPixel(x-1,y+0);
+				//e = this.getPixel(x,y);
+				f = this.getPixel(x+1,y+0);
+				g = this.getPixel(x-1,y+1);
+				h = this.getPixel(x+0,y+1);
+				i = this.getPixel(x+1,y+1);
+				var v = a & b & c & d & f & g & h & i;
+				if(v === 0) {
+					this.setPixel(x,y,1);
+				}
+			}
+		}
+	}
+}
+
+Img.prototype.dilate = function(iterations) {
+	var iterations = iterations || 1;
+	for(var i=0; i<iterations; i++) {
+		this._dilate();
+		this.threshold(1, 0xff);
+	}
+}
+
+Img.prototype.erode = function(iterations) {
+	var iterations = iterations || 1;
+	for(var i=0; i<iterations; i++) {
+		this._erode();
+		this.threshold(2, 0xff);
+	}
+}
+
+Img.prototype.normalize = function(minval, maxval) {
+	var img_min = min(this.d);
+	var img_max = max(this.d);
+	var img_scale = (img_max - img_min);
+	var output_scale = (maxval-minval);
+	console.log('img-min: ' + img_min)
+	console.log('img-max: ' + img_max)
+	console.log('img-scale: ' + img_scale)
+	console.log('output-scale: ' + output_scale)
+
+	for(var i=0; i<this.d.length; i++) {
+		var p = minval + output_scale*(this.d[i] - img_min)/img_scale;
+		this.d[i] = p;
+	}
+}
+
+Img.prototype.neighborhood = function(x,y) {
+	nw = this.getPixel(x-1,y-1) ? 1 : 0;
+	n = this.getPixel(x+0,y-1) ? 1 : 0;
+	ne = this.getPixel(x+1,y-1) ? 1 : 0;
+	w = this.getPixel(x-1,y+0) ? 1 : 0;
+	e = this.getPixel(x+1,y+0) ? 1 : 0;
+	sw = this.getPixel(x-1,y+1) ? 1 : 0;
+	s = this.getPixel(x+0,y+1) ? 1 : 0;
+	se = this.getPixel(x+1,y+1) ? 1 : 0;
+	return neighborhoodToMask(n,ne,e,se,s,sw,w,nw);
+}
+
+Img.prototype.cdt = function() {
+
+	// Create second image to store result
+	new_img = new Img(this.width, this.height);
+
+	this.threshold(0xff, 0xffff);
+	var pixel_changed = true;
+	var max_v = 0
+	var iteration = 0;
+	while(pixel_changed) {
+		iteration += 1;
+		pixel_changed = false;
+		var pixels_changed = 0;
+		for(var x = 1; x<this.width-1; x++) {
+			for(var y=1; y<this.height-1; y++) {
+				center = this.getPixel(x,y);
+				if(center === 0xffff) {
+					n = this.getPixel(x+0,y-1);
+					s = this.getPixel(x+0,y+1);
+					w = this.getPixel(x-1,y+0);
+					e = this.getPixel(x+1,y+0);
+					var v = n & s & e & w;
+					if(v === 0) {
+						new_img.setPixel(x,y,iteration);
+						this.setPixel(x,y,1);
+						pixel_changed = true;
+						pixels_changed += 1;
+					}
+				}
+			}
+		}
+		this.threshold(2, 0xffff);
+		console.log(pixels_changed);
+		console.log(iteration);
+	}
+
+	for(i=0; i<this.d.length; i++) {
+		this.d[i] = new_img.d[i];
+	}
+}
+
+Img.prototype.thin = function(lut) {
+	for(var x = 1; x<this.width-1; x++) {
+		for(var y=1; y<this.height-1; y++) {
+			center = this.getPixel(x,y);
+			if(center === 0xff) {
+				var mask = this.neighborhood(x,y)
+				if(lut[mask]) {
+					this.setPixel(x,y,1)
+				}
+			}
+		}
+	}
+	this.threshold(2, 0xff);
+}
---
+++ scripts/lut.py
@@ -0,0 +1,123 @@
+kernel = '''
+X1X
+011
+00X
+'''
+def make_skel_luts():
+    ak = '''
+000
+X1X
+111
+'''
+
+    bk = '''
+X00
+110
+X1X
+'''
+    i=0
+    names = 'abcdefghi'
+    while(i<8):
+        alut = create_lut_from_kernel(ak, rot=False)
+        print jslut(alut, 'lut_' + names[i])
+        i+=1
+        blut = create_lut_from_kernel(bk, rot=False)
+        print jslut(blut, 'lut_' + names[i])
+        i+=1
+        ak = rotate_kernel_90(ak)
+        bk = rotate_kernel_90(bk)
+
+def jslut(lut, name):
+    return 'var ' + name + ' = [' + ','.join(['1' if x else '0' for x in lut]) + ']'
+
+def create_lut_from_kernel(kernel, rot=True):
+    masks = kernel_to_masks(kernel)
+    if(rot):
+        masks_90 = rotate_masks_90(masks)
+        masks_180 = rotate_masks_90(masks_90)
+        masks_270 = rotate_masks_90(masks_180)
+       
+        ones = (masks['1'], masks_90['1'], masks_180['1'], masks_270['1'])
+        zeroes = (masks['0'], masks_90['0'], masks_180['0'], masks_270['0'])
+        dont_cares = (masks['X'], masks_90['X'], masks_180['X'], masks_270['X'])
+    else:
+        ones = (masks['1'],)
+        zeroes = (masks['0'],)
+        dont_cares = (masks['X'],)
+
+    lut = [False]*256
+
+    for mask in range(256):
+        accept = False
+        for one, zero, dc in zip(ones, zeroes, dont_cares):
+            o = mask & (~dc & 0xff)
+            z = (~mask & 0xff) & (~dc & 0xff)
+            if not (o ^ one) and (o ^ zero):
+                accept=True
+                break
+        lut[mask] = accept
+
+    return lut
+
+def check_all_kernels(standard):
+    lut = create_lut_from_kernel(standard)
+    for i in range(256):
+        print i
+        print mask_to_neighborhood(i),
+        print standard,
+        print lut[i]
+        print '-'*20
+        raw_input()
+def check_kernel(test, standard):
+    lut = create_lut_from_kernel(standard)
+    val = kernel_to_neighborhood(test)
+    print test
+    return lut[val]
+
+def rotate_masks_90(masks):
+    retval = {'0':0,'1':0,'X':0}
+    for key, mask in masks.items():
+        retval[key] = ((mask << 2) & 0xff) | ((mask >> 6))
+    return retval
+
+def rotate_kernel_90(kernel):
+    masks = kernel_to_masks(kernel)
+    rmasks = rotate_masks_90(masks)
+    return masks_to_kernel(rmasks)
+
+def kernel_to_neighborhood(kernel):
+    masks = kernel_to_masks(kernel)
+    return masks['1']
+
+def mask_to_neighborhood(mask):
+    d = {'1':mask,'0':~mask & 0xff,'X':0}
+    return masks_to_kernel(d)
+
+def kernel_to_masks(kernel):
+    kernel = kernel.strip()
+    (nw,n,ne),(w,c,e),(sw,s,se) = kernel.split('\n')
+    keys = {'1':0,'0':0,'X':0}
+    for key in keys:
+        keys[key] |= ((1 << 0) if n == key else 0)
+        keys[key] |= ((1 << 1) if ne == key else 0)
+        keys[key] |= ((1 << 2) if e == key else 0)
+        keys[key] |= ((1 << 3) if se == key else 0)
+        keys[key] |= ((1 << 4) if s == key else 0)
+        keys[key] |= ((1 << 5) if sw == key else 0)
+        keys[key] |= ((1 << 6) if w == key else 0)
+        keys[key] |= ((1 << 7) if nw == key else 0)
+    return keys
+
+def masks_to_kernel(masks):
+    kernel = [' ',' ',' ','\n',' ','1',' ','\n',' ',' ',' ']
+    for key,mask in masks.items():
+        if mask & (1 << 0): kernel[1] = key # N
+        if mask & (1 << 1): kernel[2] = key # NE
+        if mask & (1 << 2): kernel[6] = key # E
+        if mask & (1 << 3): kernel[10] = key # SE
+        if mask & (1 << 4): kernel[9] = key # S
+        if mask & (1 << 5): kernel[8] = key # SW
+        if mask & (1 << 6): kernel[4] = key # W
+        if mask & (1 << 7): kernel[0] = key # NW
+    return '\n' + ''.join(kernel) + '\n'
+
